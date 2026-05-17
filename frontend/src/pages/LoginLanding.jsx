7b1f7524d472d40fdb72c4aae5c0073e227969b7
sobi-sensei <suhaibawan666@gmail.com>
2026-05-18 00:49:22 +0500

    Added full Login UI

--- frontend/src/pages/LoginLanding.jsx
+++ frontend/src/pages/LoginLanding.jsx
@@ -3,6 +3,7 @@ import { ShieldIcon, UserIcon, ArrowRightIcon } from 'lucide-react'
 
 
 import LoginSidePanel from "../components/LoginSidePanel"
+import LoginForm from "../components/LoginForm"
 
 const LoginLanding = () => {
 
@@ -31,7 +32,7 @@ const LoginLanding = () => {
 
                 {/* Header */}
                 <div className="mb-20 text-center md:text-left">
-                    <h2 className="text-3xl font-medium text-slate-900 tracking-tight mb-1">Welcome</h2>
+                    <h2 className="text-3xl font-medium text-slate-900 tracking-tight">Welcome</h2>
                     <p className="text-slate-500">Choose your designated role.</p>
                 </div>
 
@@ -40,7 +41,7 @@ const LoginLanding = () => {
                     {portalOptions.map((portal)=>(
                         <Link 
                         key={portal.to} 
-                        to={portal.to} className="group block bg-slate-200 border border-slate-50 rounded-lg p-5 sm:p-6 transition-all duration-300 hover:border-indigo-400 hover:bg-indigo-100">
+                        to={portal.to} className="group block bg-slate-100 border border-slate-50 rounded-lg p-5 sm:p-6 transition-all duration-300 hover:border-indigo-400 hover:bg-indigo-100">
                             <div className="relative z-10 flex items-center justify-between gap-4 sm:gap-5">
                                 <h3 className="text-md text-slate-800 group-hover:text-indigo-600 mb-1 transition-colors">{portal.title}</h3>
                                 <ArrowRightIcon className="w-5 h-5 text-slate-400 group-hover:translate-x-1 transition-all duration-300"/>
